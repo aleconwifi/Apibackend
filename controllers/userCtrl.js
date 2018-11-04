6d6bdaf1271317639a0815686e100fe302dc4b8f
Alejandro Marcano <alejandro.marcano@correo.unimet.edu.ve>
2018-11-04 01:04:59 -0400

    anadiendo rutas autenticacion

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -24,3 +24,28 @@ exports.createUser = (req, res, next) => {
     })(req, res, next);
 
 }
+
+exports.loginUser = (req, res, next) => {
+    //Validacion para algun campo vacio
+    if (req.body.email === undefined || req.body.password === undefined) {
+        return res.status(200).json({ error: 'No puedes poner campos vacios' });
+    }
+
+    //Validacion si deja los 2 campos en blanco
+    if (req.body.email === '' || req.body.password === '') {
+        return res.status(200).json({ error: 'No puedes poner campos vacios' });
+    }
+
+    passport.authenticate('local-login', (err, user, info) => {
+        if (err) {
+            return res.status(200).json({ error: err }); //mostrar error 500 si no autentica
+        }
+
+        if (info) {
+            return res.status(200).json({ error: info });
+        }
+        //si es pasa el local-singup es creado el usuario (en passport se crea el usuario y se guarda)
+        return res.status(201).json({ message: 'Usuario logeado', user: user });
+    })(req, res, next);
+
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const UserCtrl = require('../controllers/userCtrl');
 
 router.post('/signup/user', UserCtrl.createUser);
+router.post('/login/user', UserCtrl.loginUser);
 
 module.exports = router;
 
