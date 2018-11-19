0b2115986820d4adecc912c5dd08abe29496fbd4
Alejandro Marcano <alejandro.marcano@correo.unimet.edu.ve>
2018-11-19 18:24:11 -0400

    populate

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -55,7 +55,8 @@ exports.loginUser = (req, res, next) => {
 
 exports.homePage = async(req, res) => {
     const result = await User.findOne({ 'email': req.params.email }, { 'password': 0 })
-        .populate("eventos.evento");
+        .populate("eventos.evento")
+        .populate("asistires.asistire");
 
     return res.status(200).json({ user: result });
 }
